894e7f637dc0d349fe1480f3ba584f31bd5979af
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2017-07-24 21:58:26 -1000

    created toggle switch for secret menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var getName = document.getElementsByClassName("name");
+for(var i = 0; i < getName.length; i++){
+	//console.log(getName[i]);
+	getName[i].addEventListener("click", showSecrets);
+}
 
+function showSecrets(){
+	var menuElem = this.querySelectorAll(".menu")[0];
+	if(menuElem.style.display === "none"){
+		menuElem.style.display = "block";
+	}else{
+		menuElem.style.display = "none";
+	}
+}
